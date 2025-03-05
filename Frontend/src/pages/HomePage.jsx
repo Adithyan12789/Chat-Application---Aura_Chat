844ba276cc9ef190365b20c9c6d35059da77decd
Adithyan <Adithiruthiparambil12@gmail.com>
2025-03-05 22:58:29 +0530

    fixed import name

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import { useChatStore } from "../store/useChatStore";
 
 import Sidebar from "../components/Sidebar";
 import ChatContainer from "../components/ChatContainer";
-import NoChatSelected from "../components/NoCHatSelected";
+import NoChatSelected from "../components/NoChatSelected";
 
 const HomePage = () => {
   const { selectedUser } = useChatStore();
